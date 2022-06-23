57c4279f7b8c49d09f2dfd2198a06b1d5a583ff7
Bhaskar100-hub <bhaskarwary7@gmail.com>
2022-06-23 21:53:15 +0530

    validating the input username and password

--- app/src/main/java/wary/bhaskar/shoppy/RegisterFragment.kt
+++ app/src/main/java/wary/bhaskar/shoppy/RegisterFragment.kt
@@ -1,13 +1,21 @@
 package wary.bhaskar.shoppy
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
 
 class RegisterFragment : Fragment() {
+    private lateinit var username: EditText
+    private lateinit var password: EditText
+    private lateinit var cnfPassword: EditText
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -15,11 +23,59 @@ class RegisterFragment : Fragment() {
         // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_register, container, false)
 
+        username = view.findViewById(R.id.reg_username)
+        password = view.findViewById(R.id.reg_password)
+        cnfPassword = view.findViewById(R.id.reg_cnf_password)
+
         view.findViewById<Button>(R.id.btn_login_reg).setOnClickListener {
             var navRegister = activity as FragmentNavigation
             navRegister.navigateFrag(LoginFragment(), false)
         }
 
+        view.findViewById<Button>(R.id.btn_register_reg).setOnClickListener {
+            validateEmptyForm()
+        }
         return view
     }
+
+    private fun validateEmptyForm() {
+        val icon = AppCompatResources.getDrawable(requireContext(),
+            R.drawable.ic_error
+        )
+
+        icon?.setBounds(0,0,icon.intrinsicWidth, icon.intrinsicHeight)
+        when {
+            TextUtils.isEmpty(username.text.toString().trim()) -> {
+                username.setError("Please enter username", icon)
+            }
+            TextUtils.isEmpty(password.text.toString().trim()) -> {
+                password.setError("Please enter password", icon)
+            }
+            TextUtils.isEmpty(cnfPassword.text.toString().trim()) -> {
+                cnfPassword.setError("Please enter password again", icon)
+            }
+
+            username.text.toString().isNotEmpty() &&
+                    password.text.toString().isNotEmpty() &&
+                    cnfPassword.text.toString().isNotEmpty() ->
+            {
+                if (
+                    username.text.toString().matches(Regex("[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}\$"))) {
+                    if (password.text.toString().length >= 5) {
+                        if (password.text.toString() == cnfPassword.text.toString()) {
+                            Toast.makeText(context, "Register Successfully", Toast.LENGTH_SHORT).show()
+                        } else {
+                            cnfPassword.setError("Password didn't match", icon)
+                        }
+                    } else {
+                        password.setError("Please enter at least 5 characters", icon)
+                    }
+                }
+                else {
+                    username.setError("Please enter valid Email Id",icon)
+                }
+            }
+        }
+    }
+
 }
