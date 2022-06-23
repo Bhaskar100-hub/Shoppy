2a400938ea8bed78f6fae51aa0116fb9822ad5f8
Bhaskar100-hub <bhaskarwary7@gmail.com>
2022-06-23 21:11:05 +0530

    Created Register Page

--- app/src/main/java/wary/bhaskar/shoppy/MainActivity.kt
+++ app/src/main/java/wary/bhaskar/shoppy/MainActivity.kt
@@ -2,8 +2,9 @@ package wary.bhaskar.shoppy
 
 import  androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentNavigation {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -11,5 +12,16 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .add(R.id.container, LoginFragment())
             .commit()
+    }
+
+    override fun navigateFrag(fragment: Fragment, addToStack: Boolean) {
+        val transaction = supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, fragment)
+
+        if (addToStack) {
+            transaction.addToBackStack(null)
+        }
+        transaction.commit()
     }
 }
--- app/src/main/java/wary/bhaskar/shoppy/RegisterFragment.kt
+++ app/src/main/java/wary/bhaskar/shoppy/RegisterFragment.kt
@@ -7,17 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 
-class LoginFragment : Fragment() {
+class RegisterFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        var view = inflater.inflate(R.layout.fragment_login, container, false)
-        view.findViewById<Button>(R.id.btn_register).setOnClickListener {
+        var view = inflater.inflate(R.layout.fragment_register, container, false)
+
+        view.findViewById<Button>(R.id.btn_login_reg).setOnClickListener {
             var navRegister = activity as FragmentNavigation
-            navRegister.navigateFrag(RegisterFragment(), false)
+            navRegister.navigateFrag(LoginFragment(), false)
         }
+
         return view
     }
 }
